da79724ba3af4ee75e676b7768b480e9bc693cf6
yuzumone <yuzumone@outlook.com>
2025-05-08 17:44:39 +0900

    Update freezed

--- lib/ui/home/home_view_state.freezed.dart
+++ lib/ui/home/home_view_state.freezed.dart
@@ -1,5 +1,8 @@
+// dart format width=80
+// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'home_view_state.dart';
 
@@ -7,317 +10,184 @@ part of 'home_view_state.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods',
-);
-
 /// @nodoc
-class _$HomeViewStateTearOff {
-  const _$HomeViewStateTearOff();
+mixin _$HomeViewState implements DiagnosticableTreeMixin {
 
-  _HomeViewState call({
-    int pageIndex = 0,
-    List<File> files = const [],
-    List<String> urls = const [],
-    List<String> todoKeywords = const [],
-    List<String> doneKeywords = const [],
-    int weekDiff = 0,
-  }) {
-    return _HomeViewState(
-      pageIndex: pageIndex,
-      files: files,
-      urls: urls,
-      todoKeywords: todoKeywords,
-      doneKeywords: doneKeywords,
-      weekDiff: weekDiff,
-    );
-  }
+ int get pageIndex; List<File> get files; List<String> get urls; List<String> get todoKeywords; List<String> get doneKeywords; int get weekDiff;
+/// Create a copy of HomeViewState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$HomeViewStateCopyWith<HomeViewState> get copyWith => _$HomeViewStateCopyWithImpl<HomeViewState>(this as HomeViewState, _$identity);
+
+
+@override
+void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+  properties
+    ..add(DiagnosticsProperty('type', 'HomeViewState'))
+    ..add(DiagnosticsProperty('pageIndex', pageIndex))..add(DiagnosticsProperty('files', files))..add(DiagnosticsProperty('urls', urls))..add(DiagnosticsProperty('todoKeywords', todoKeywords))..add(DiagnosticsProperty('doneKeywords', doneKeywords))..add(DiagnosticsProperty('weekDiff', weekDiff));
+}
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is HomeViewState&&(identical(other.pageIndex, pageIndex) || other.pageIndex == pageIndex)&&const DeepCollectionEquality().equals(other.files, files)&&const DeepCollectionEquality().equals(other.urls, urls)&&const DeepCollectionEquality().equals(other.todoKeywords, todoKeywords)&&const DeepCollectionEquality().equals(other.doneKeywords, doneKeywords)&&(identical(other.weekDiff, weekDiff) || other.weekDiff == weekDiff));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,pageIndex,const DeepCollectionEquality().hash(files),const DeepCollectionEquality().hash(urls),const DeepCollectionEquality().hash(todoKeywords),const DeepCollectionEquality().hash(doneKeywords),weekDiff);
+
+@override
+String toString({ DiagnosticLevel minLevel = DiagnosticLevel.info }) {
+  return 'HomeViewState(pageIndex: $pageIndex, files: $files, urls: $urls, todoKeywords: $todoKeywords, doneKeywords: $doneKeywords, weekDiff: $weekDiff)';
+}
+
+
 }
 
 /// @nodoc
-const $HomeViewState = _$HomeViewStateTearOff();
+abstract mixin class $HomeViewStateCopyWith<$Res>  {
+  factory $HomeViewStateCopyWith(HomeViewState value, $Res Function(HomeViewState) _then) = _$HomeViewStateCopyWithImpl;
+@useResult
+$Res call({
+ int pageIndex, List<File> files, List<String> urls, List<String> todoKeywords, List<String> doneKeywords, int weekDiff
+});
 
-/// @nodoc
-mixin _$HomeViewState {
-  int get pageIndex => throw _privateConstructorUsedError;
-  List<File> get files => throw _privateConstructorUsedError;
-  List<String> get urls => throw _privateConstructorUsedError;
-  List<String> get todoKeywords => throw _privateConstructorUsedError;
-  List<String> get doneKeywords => throw _privateConstructorUsedError;
-  int get weekDiff => throw _privateConstructorUsedError;
 
-  @JsonKey(ignore: true)
-  $HomeViewStateCopyWith<HomeViewState> get copyWith =>
-      throw _privateConstructorUsedError;
+
+
 }
-
-/// @nodoc
-abstract class $HomeViewStateCopyWith<$Res> {
-  factory $HomeViewStateCopyWith(
-    HomeViewState value,
-    $Res Function(HomeViewState) then,
-  ) = _$HomeViewStateCopyWithImpl<$Res>;
-  $Res call({
-    int pageIndex,
-    List<File> files,
-    List<String> urls,
-    List<String> todoKeywords,
-    List<String> doneKeywords,
-    int weekDiff,
-  });
-}
-
 /// @nodoc
 class _$HomeViewStateCopyWithImpl<$Res>
     implements $HomeViewStateCopyWith<$Res> {
-  _$HomeViewStateCopyWithImpl(this._value, this._then);
+  _$HomeViewStateCopyWithImpl(this._self, this._then);
 
-  final HomeViewState _value;
-  // ignore: unused_field
+  final HomeViewState _self;
   final $Res Function(HomeViewState) _then;
 
-  @override
-  $Res call({
-    Object? pageIndex = freezed,
-    Object? files = freezed,
-    Object? urls = freezed,
-    Object? todoKeywords = freezed,
-    Object? doneKeywords = freezed,
-    Object? weekDiff = freezed,
-  }) {
-    return _then(
-      _value.copyWith(
-        pageIndex:
-            pageIndex == freezed
-                ? _value.pageIndex
-                : pageIndex // ignore: cast_nullable_to_non_nullable
-                    as int,
-        files:
-            files == freezed
-                ? _value.files
-                : files // ignore: cast_nullable_to_non_nullable
-                    as List<File>,
-        urls:
-            urls == freezed
-                ? _value.urls
-                : urls // ignore: cast_nullable_to_non_nullable
-                    as List<String>,
-        todoKeywords:
-            todoKeywords == freezed
-                ? _value.todoKeywords
-                : todoKeywords // ignore: cast_nullable_to_non_nullable
-                    as List<String>,
-        doneKeywords:
-            doneKeywords == freezed
-                ? _value.doneKeywords
-                : doneKeywords // ignore: cast_nullable_to_non_nullable
-                    as List<String>,
-        weekDiff:
-            weekDiff == freezed
-                ? _value.weekDiff
-                : weekDiff // ignore: cast_nullable_to_non_nullable
-                    as int,
-      ),
-    );
-  }
+/// Create a copy of HomeViewState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? pageIndex = null,Object? files = null,Object? urls = null,Object? todoKeywords = null,Object? doneKeywords = null,Object? weekDiff = null,}) {
+  return _then(_self.copyWith(
+pageIndex: null == pageIndex ? _self.pageIndex : pageIndex // ignore: cast_nullable_to_non_nullable
+as int,files: null == files ? _self.files : files // ignore: cast_nullable_to_non_nullable
+as List<File>,urls: null == urls ? _self.urls : urls // ignore: cast_nullable_to_non_nullable
+as List<String>,todoKeywords: null == todoKeywords ? _self.todoKeywords : todoKeywords // ignore: cast_nullable_to_non_nullable
+as List<String>,doneKeywords: null == doneKeywords ? _self.doneKeywords : doneKeywords // ignore: cast_nullable_to_non_nullable
+as List<String>,weekDiff: null == weekDiff ? _self.weekDiff : weekDiff // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
+}
+
+}
+
+
+/// @nodoc
+
+
+class _HomeViewState with DiagnosticableTreeMixin implements HomeViewState {
+  const _HomeViewState({this.pageIndex = 0, final  List<File> files = const [], final  List<String> urls = const [], final  List<String> todoKeywords = const [], final  List<String> doneKeywords = const [], this.weekDiff = 0}): _files = files,_urls = urls,_todoKeywords = todoKeywords,_doneKeywords = doneKeywords;
+  
+
+@override@JsonKey() final  int pageIndex;
+ final  List<File> _files;
+@override@JsonKey() List<File> get files {
+  if (_files is EqualUnmodifiableListView) return _files;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_files);
+}
+
+ final  List<String> _urls;
+@override@JsonKey() List<String> get urls {
+  if (_urls is EqualUnmodifiableListView) return _urls;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_urls);
+}
+
+ final  List<String> _todoKeywords;
+@override@JsonKey() List<String> get todoKeywords {
+  if (_todoKeywords is EqualUnmodifiableListView) return _todoKeywords;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_todoKeywords);
+}
+
+ final  List<String> _doneKeywords;
+@override@JsonKey() List<String> get doneKeywords {
+  if (_doneKeywords is EqualUnmodifiableListView) return _doneKeywords;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_doneKeywords);
+}
+
+@override@JsonKey() final  int weekDiff;
+
+/// Create a copy of HomeViewState
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$HomeViewStateCopyWith<_HomeViewState> get copyWith => __$HomeViewStateCopyWithImpl<_HomeViewState>(this, _$identity);
+
+
+@override
+void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+  properties
+    ..add(DiagnosticsProperty('type', 'HomeViewState'))
+    ..add(DiagnosticsProperty('pageIndex', pageIndex))..add(DiagnosticsProperty('files', files))..add(DiagnosticsProperty('urls', urls))..add(DiagnosticsProperty('todoKeywords', todoKeywords))..add(DiagnosticsProperty('doneKeywords', doneKeywords))..add(DiagnosticsProperty('weekDiff', weekDiff));
+}
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _HomeViewState&&(identical(other.pageIndex, pageIndex) || other.pageIndex == pageIndex)&&const DeepCollectionEquality().equals(other._files, _files)&&const DeepCollectionEquality().equals(other._urls, _urls)&&const DeepCollectionEquality().equals(other._todoKeywords, _todoKeywords)&&const DeepCollectionEquality().equals(other._doneKeywords, _doneKeywords)&&(identical(other.weekDiff, weekDiff) || other.weekDiff == weekDiff));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,pageIndex,const DeepCollectionEquality().hash(_files),const DeepCollectionEquality().hash(_urls),const DeepCollectionEquality().hash(_todoKeywords),const DeepCollectionEquality().hash(_doneKeywords),weekDiff);
+
+@override
+String toString({ DiagnosticLevel minLevel = DiagnosticLevel.info }) {
+  return 'HomeViewState(pageIndex: $pageIndex, files: $files, urls: $urls, todoKeywords: $todoKeywords, doneKeywords: $doneKeywords, weekDiff: $weekDiff)';
+}
+
+
 }
 
 /// @nodoc
-abstract class _$HomeViewStateCopyWith<$Res>
-    implements $HomeViewStateCopyWith<$Res> {
-  factory _$HomeViewStateCopyWith(
-    _HomeViewState value,
-    $Res Function(_HomeViewState) then,
-  ) = __$HomeViewStateCopyWithImpl<$Res>;
-  @override
-  $Res call({
-    int pageIndex,
-    List<File> files,
-    List<String> urls,
-    List<String> todoKeywords,
-    List<String> doneKeywords,
-    int weekDiff,
-  });
-}
+abstract mixin class _$HomeViewStateCopyWith<$Res> implements $HomeViewStateCopyWith<$Res> {
+  factory _$HomeViewStateCopyWith(_HomeViewState value, $Res Function(_HomeViewState) _then) = __$HomeViewStateCopyWithImpl;
+@override @useResult
+$Res call({
+ int pageIndex, List<File> files, List<String> urls, List<String> todoKeywords, List<String> doneKeywords, int weekDiff
+});
 
+
+
+
+}
 /// @nodoc
 class __$HomeViewStateCopyWithImpl<$Res>
-    extends _$HomeViewStateCopyWithImpl<$Res>
     implements _$HomeViewStateCopyWith<$Res> {
-  __$HomeViewStateCopyWithImpl(
-    _HomeViewState _value,
-    $Res Function(_HomeViewState) _then,
-  ) : super(_value, (v) => _then(v as _HomeViewState));
+  __$HomeViewStateCopyWithImpl(this._self, this._then);
 
-  @override
-  _HomeViewState get _value => super._value as _HomeViewState;
+  final _HomeViewState _self;
+  final $Res Function(_HomeViewState) _then;
 
-  @override
-  $Res call({
-    Object? pageIndex = freezed,
-    Object? files = freezed,
-    Object? urls = freezed,
-    Object? todoKeywords = freezed,
-    Object? doneKeywords = freezed,
-    Object? weekDiff = freezed,
-  }) {
-    return _then(
-      _HomeViewState(
-        pageIndex:
-            pageIndex == freezed
-                ? _value.pageIndex
-                : pageIndex // ignore: cast_nullable_to_non_nullable
-                    as int,
-        files:
-            files == freezed
-                ? _value.files
-                : files // ignore: cast_nullable_to_non_nullable
-                    as List<File>,
-        urls:
-            urls == freezed
-                ? _value.urls
-                : urls // ignore: cast_nullable_to_non_nullable
-                    as List<String>,
-        todoKeywords:
-            todoKeywords == freezed
-                ? _value.todoKeywords
-                : todoKeywords // ignore: cast_nullable_to_non_nullable
-                    as List<String>,
-        doneKeywords:
-            doneKeywords == freezed
-                ? _value.doneKeywords
-                : doneKeywords // ignore: cast_nullable_to_non_nullable
-                    as List<String>,
-        weekDiff:
-            weekDiff == freezed
-                ? _value.weekDiff
-                : weekDiff // ignore: cast_nullable_to_non_nullable
-                    as int,
-      ),
-    );
-  }
+/// Create a copy of HomeViewState
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? pageIndex = null,Object? files = null,Object? urls = null,Object? todoKeywords = null,Object? doneKeywords = null,Object? weekDiff = null,}) {
+  return _then(_HomeViewState(
+pageIndex: null == pageIndex ? _self.pageIndex : pageIndex // ignore: cast_nullable_to_non_nullable
+as int,files: null == files ? _self._files : files // ignore: cast_nullable_to_non_nullable
+as List<File>,urls: null == urls ? _self._urls : urls // ignore: cast_nullable_to_non_nullable
+as List<String>,todoKeywords: null == todoKeywords ? _self._todoKeywords : todoKeywords // ignore: cast_nullable_to_non_nullable
+as List<String>,doneKeywords: null == doneKeywords ? _self._doneKeywords : doneKeywords // ignore: cast_nullable_to_non_nullable
+as List<String>,weekDiff: null == weekDiff ? _self.weekDiff : weekDiff // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
 }
 
-/// @nodoc
-class _$_HomeViewState with DiagnosticableTreeMixin implements _HomeViewState {
-  const _$_HomeViewState({
-    this.pageIndex = 0,
-    this.files = const [],
-    this.urls = const [],
-    this.todoKeywords = const [],
-    this.doneKeywords = const [],
-    this.weekDiff = 0,
-  });
 
-  @JsonKey(defaultValue: 0)
-  @override
-  final int pageIndex;
-  @JsonKey(defaultValue: const [])
-  @override
-  final List<File> files;
-  @JsonKey(defaultValue: const [])
-  @override
-  final List<String> urls;
-  @JsonKey(defaultValue: const [])
-  @override
-  final List<String> todoKeywords;
-  @JsonKey(defaultValue: const [])
-  @override
-  final List<String> doneKeywords;
-  @JsonKey(defaultValue: 0)
-  @override
-  final int weekDiff;
-
-  @override
-  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
-    return 'HomeViewState(pageIndex: $pageIndex, files: $files, urls: $urls, todoKeywords: $todoKeywords, doneKeywords: $doneKeywords, weekDiff: $weekDiff)';
-  }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties
-      ..add(DiagnosticsProperty('type', 'HomeViewState'))
-      ..add(DiagnosticsProperty('pageIndex', pageIndex))
-      ..add(DiagnosticsProperty('files', files))
-      ..add(DiagnosticsProperty('urls', urls))
-      ..add(DiagnosticsProperty('todoKeywords', todoKeywords))
-      ..add(DiagnosticsProperty('doneKeywords', doneKeywords))
-      ..add(DiagnosticsProperty('weekDiff', weekDiff));
-  }
-
-  @override
-  bool operator ==(dynamic other) {
-    return identical(this, other) ||
-        (other is _HomeViewState &&
-            (identical(other.pageIndex, pageIndex) ||
-                const DeepCollectionEquality().equals(
-                  other.pageIndex,
-                  pageIndex,
-                )) &&
-            (identical(other.files, files) ||
-                const DeepCollectionEquality().equals(other.files, files)) &&
-            (identical(other.urls, urls) ||
-                const DeepCollectionEquality().equals(other.urls, urls)) &&
-            (identical(other.todoKeywords, todoKeywords) ||
-                const DeepCollectionEquality().equals(
-                  other.todoKeywords,
-                  todoKeywords,
-                )) &&
-            (identical(other.doneKeywords, doneKeywords) ||
-                const DeepCollectionEquality().equals(
-                  other.doneKeywords,
-                  doneKeywords,
-                )) &&
-            (identical(other.weekDiff, weekDiff) ||
-                const DeepCollectionEquality().equals(
-                  other.weekDiff,
-                  weekDiff,
-                )));
-  }
-
-  @override
-  int get hashCode =>
-      runtimeType.hashCode ^
-      const DeepCollectionEquality().hash(pageIndex) ^
-      const DeepCollectionEquality().hash(files) ^
-      const DeepCollectionEquality().hash(urls) ^
-      const DeepCollectionEquality().hash(todoKeywords) ^
-      const DeepCollectionEquality().hash(doneKeywords) ^
-      const DeepCollectionEquality().hash(weekDiff);
-
-  @JsonKey(ignore: true)
-  @override
-  _$HomeViewStateCopyWith<_HomeViewState> get copyWith =>
-      __$HomeViewStateCopyWithImpl<_HomeViewState>(this, _$identity);
 }
 
-abstract class _HomeViewState implements HomeViewState {
-  const factory _HomeViewState({
-    int pageIndex,
-    List<File> files,
-    List<String> urls,
-    List<String> todoKeywords,
-    List<String> doneKeywords,
-    int weekDiff,
-  }) = _$_HomeViewState;
-
-  @override
-  int get pageIndex => throw _privateConstructorUsedError;
-  @override
-  List<File> get files => throw _privateConstructorUsedError;
-  @override
-  List<String> get urls => throw _privateConstructorUsedError;
-  @override
-  List<String> get todoKeywords => throw _privateConstructorUsedError;
-  @override
-  List<String> get doneKeywords => throw _privateConstructorUsedError;
-  @override
-  int get weekDiff => throw _privateConstructorUsedError;
-  @override
-  @JsonKey(ignore: true)
-  _$HomeViewStateCopyWith<_HomeViewState> get copyWith =>
-      throw _privateConstructorUsedError;
-}
+// dart format on
