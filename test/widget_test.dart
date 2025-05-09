cb45e91ef4c060911bf1c86ef16b8404f3fc0b3d
yuzumone <yuzumone@outlook.com>
2025-05-09 22:00:38 +0900

    Fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,17 +3,27 @@ import 'package:orgcal/data/repository/file_repository.dart';
 import 'package:orgcal/data/repository/preference_repository.dart';
 import 'package:orgcal/main.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
 
 void main() {
-  testWidgets('App widget test', (tester) async {
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          Provider<FileRepository>(create: (_) => FileRepository()),
-          Provider<PreferenceRepository>(create: (_) => PreferenceRepository()),
-        ],
-        child: MyApp(),
-      ),
-    );
+  group('SharedPreferences saveLocale', () {
+    setUpAll(() async {
+      SharedPreferencesAsyncPlatform.instance =
+          InMemorySharedPreferencesAsync.empty();
+    });
+    testWidgets('App widget test', (tester) async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            Provider<FileRepository>(create: (_) => FileRepository()),
+            Provider<PreferenceRepository>(
+              create: (_) => PreferenceRepository(),
+            ),
+          ],
+          child: MyApp(),
+        ),
+      );
+    });
   });
 }
