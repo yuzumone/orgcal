81861fafb53e339624f8316d0177490ceb7a2e0f
yuzumone <yuzumone@outlook.com>
2025-06-03 21:08:44 +0900

    Set ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 android {
     namespace = "net.yuzumone.orgcal"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
